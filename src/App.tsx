d143d03310cc1d3cacb30e2f87661267a2ac56a7
shizukani9 <tatianamayorga9999@gmail.com>
2025-02-11 20:53:49 -0400

    name

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App: React.FC = () => {
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Button label="Click Me" />
-      <Avatar name="Tatiana Mayorga" src="" />
+      <Avatar name="Cesar Galindo" src="" />
     </ThemeProvider>
   );
 };
